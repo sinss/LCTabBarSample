84777237903ca672aafb8e071155d7a58c3d4e13
Leo Chang <sinss0000@gmail.com>
2015-05-30 22:59:16 +0800

    Initial Commit

---
+++ LCCustomTabBarSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCCustomTabBarSample
+//
+//  Created by Leo on 5/30/15.
+//  Copyright (c) 2015 Perfectidea Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCCustomTabBarSample/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  LCCustomTabBarSample
+//
+//  Created by Leo on 5/30/15.
+//  Copyright (c) 2015 Perfectidea Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
